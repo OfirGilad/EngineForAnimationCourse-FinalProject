b959f1a366c7db1c1f9f42612a2da5e563f3a4d1
Ofir Gilad <ofir.gilad9631@gmail.com>
2023-02-07 23:44:24 +0200

    sound manager in progress

---
+++ tutorial/FinalProject/SoundManager.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Ofir Gilad on 07/02/2023.
+//
+
+#include "SoundManager.h"
+
+//SoundManager::SoundManager() {
+//    engine = createIrrKlangDevice();
+//
+//    if (!engine) {
+//        return;
+//    }
+//}
+//
+//void SoundManager::PlayCreditsSound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/credits.mp3", true);
+//}
+//
+//void SoundManager::PlayGameOverSound() {
+//
+//    engine->play2D("sounds/game_over.mp3", false);
+//}
+//
+//void SoundManager::PlayGoldObjectSound() {
+//    engine->play2D("sounds/gold_object.mp3", false);
+//}
+//
+//void SoundManager::PlayHallOfFameSound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/hall_of_fame.mp3", true);
+//}
+//
+//void SoundManager::PlayHealthObjectSound() {
+//    engine->play2D("sounds/health_object.mp3", false);
+//}
+//
+//void SoundManager::PlayHitSound() {
+//    engine->play2D("sounds/hit.mp3", false);
+//}
+//
+//void SoundManager::PlayOpeningThemeSound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/opening_theme.mp3", true);
+//}
+//
+//void SoundManager::PlayScoreObjectSound() {
+//    engine->play2D("sounds/score_object.mp3", false);
+//}
+//
+//void SoundManager::PlayShopSound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/shop.mp3", true);
+//}
+//
+//void SoundManager::PlayStageCompleteSound() {
+//    engine->play2D("sounds/stage_complete.mp3", false);
+//}
+//
+//void SoundManager::PlayStage1Sound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/stage1.mp3", true);
+//}
+//
+//void SoundManager::PlayStage2Sound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/stage2.mp3", true);
+//}
+//
+//void SoundManager::PlayStage3Sound() {
+//    engine->drop();
+//    engine = createIrrKlangDevice();
+//    engine->play2D("sounds/stage3.mp3", true);
+//}
---
+++ tutorial/FinalProject/SoundManager.h
@@ -0,0 +1,36 @@
+//
+// Created by Ofir Gilad on 07/02/2023.
+//
+
+#ifndef ENGINEFORANIMATIONCOURSE_FINALPROJECT_SOUNDMANAGER_H
+#define ENGINEFORANIMATIONCOURSE_FINALPROJECT_SOUNDMANAGER_H
+
+#endif //ENGINEFORANIMATIONCOURSE_FINALPROJECT_SOUNDMANAGER_H
+
+//#include <irrKlang.h>
+#include <string>
+
+//using namespace irrklang;
+
+class SoundManager
+{
+public:
+    //SoundManager();
+
+    //void PlayCreditsSound();
+    //void PlayGameOverSound();
+    //void PlayHallOfFameSound();
+    //void PlayHitSound();
+    //void PlayOpeningThemeSound();
+    //void PlayShopSound();
+    //void PlayStageCompleteSound();
+    //void PlayStage1Sound();
+    //void PlayStage2Sound();
+    //void PlayStage3Sound();
+
+    //void PlayGoldObjectSound();
+    //void PlayHealthObjectSound();
+    //void PlayScoreObjectSound();
+
+    //ISoundEngine *engine;
+};
